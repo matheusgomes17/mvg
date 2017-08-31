66823250f0f866b9947464d9d1c06403071955e2
Matheus Gomes <mathvicentin@gmail.com>
2017-08-30 21:57:24 -0300

    Refatorando estrutura do projeto

---
+++ app/Support/Http/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MVG\Support\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+/**
+ * Class Controller
+ * @package MVG\Support\Http\Controllers
+ */
+abstract class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
